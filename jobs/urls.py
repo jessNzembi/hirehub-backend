e82dcdeb73d4f5ea3f0296fe8deff44a98347096
Jess <jessgatura@gmail.com>
2024-04-12 11:57:41 +0300

    added job urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import add_job, delete_job, job_details, update_job
+from .views import add_job, all_jobs, delete_job, job_details, update_job
 
 urlpatterns = [
     path('add/', add_job, name='add'),
     path('details/<int:user_id>/', job_details),
     path('update/<int:job_id>/', update_job),
+    path('all/', all_jobs),
     path('delete/<int:job_id>/', delete_job),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -27,7 +27,7 @@ def add_job(request):
 def update_job(request, job_id):
     try:
         job = Jobs.objects.get(id=job_id)
-        serializer = JobSerializer(job, data=request.data)
+        serializer = JobSerializer(job, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -35,6 +35,14 @@ def update_job(request, job_id):
     except Jobs.DoesNotExist:
         return Response({'message': 'Job not found'}, status=status.HTTP_404_NOT_FOUND)
 
+@api_view(['GET'])
+def all_jobs(request):
+    try:
+        jobs = Jobs.objects.all()
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Jobs.DoesNotExist:
+        return Response({'message': 'No jobs found'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['DELETE'])
 def delete_job(request, job_id):

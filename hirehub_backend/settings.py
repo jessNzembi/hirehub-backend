343344a02de1a161885b158af8771f14dc98e6c8
Jess <jessgatura@gmail.com>
2024-04-14 22:53:02 +0300

    deployment test 2

--- hirehub_backend/settings.py
+++ hirehub_backend/settings.py
@@ -93,18 +93,15 @@ WSGI_APPLICATION = 'hirehub_backend.wsgi.application'
 #     }
 # }
 
-# Configure Postgres database based on connection string of the libpq Keyword/Value form
-# https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-# Uncomment the following lines for App Service
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# Configure Postgres database for local development
+#   Set these environment variables in the .env file for this project.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': os.environ.get('DBNAME'),
+        'HOST': os.environ.get('DBHOST'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
     }
 }
 
